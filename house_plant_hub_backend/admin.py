fcc8f90e1c128c7127ad4da75ab4c8c20a541860
Glen Sharp <glen.sharp@ibm.com>
2025-04-08 11:58:36 +0100

    feat: updated admin configuration and dockerignore

--- house_plant_hub_backend/admin.py
+++ house_plant_hub_backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from house_plant_hub_backend import models
+
+admin.site.register(models.Plant)
+admin.site.register(models.MoistureReading)
