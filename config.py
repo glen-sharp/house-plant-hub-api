b4c5f15053f6788e990fb804451c73f109e008a5
Glen Sharp <glen.sharp@ibm.com>
2025-04-08 13:18:53 +0100

    feat: updated config

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ LOG_LEVEL = "INFO"
 ORIGIN = os.environ.get("ORIGIN", "239.255.255.250")
 
 DB_NAME = os.environ.get("DB_NAME", "house-plant-hub")
-USER = os.environ.get("USER", "pi4b")
+USER = "pi4b"
 PASSWORD = os.environ.get("PASSWORD", "password")
 HOST = os.environ.get("HOST", "192.168.0.152")
