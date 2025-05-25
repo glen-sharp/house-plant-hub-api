7e066c70167564d76e8a4a05454f483859d94875
Glen Sharp <glen.sharp@ibm.com>
2025-05-25 20:14:18 +0100

    feat: added secret key to github and genereated new key

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 LOG_LEVEL = "INFO"
 
@@ -8,6 +11,8 @@ DB_NAME = os.environ.get("DB_NAME", "house-plant-hub")
 USERNAME = os.environ.get("USERNAME", "pi4b")
 PASSWORD = os.environ.get("PASSWORD", "password")
 HOST = os.environ.get("HOST", "192.168.0.152")
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 FULL_MOISTURE_LEVEL = 320
 NO_MOISTURE_LEVEL = 790
--- house_plant_hub_api/settings.py
+++ house_plant_hub_api/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e-4=l#5htfn85e%)cl2g17zh&d&8hpgg4n30c_k8vawyu@abg5"
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    config.ORIGIN,
     "127.0.0.1",
     "localhost",
     "0.0.0.0",
@@ -35,6 +34,7 @@ ALLOWED_HOSTS = [
     "192.168.0.152",
     "192.168.0.153",
     "houseplant-hub-api.glen-sharp.uk",
+    "houseplant-hub.glen-sharp.uk",
 ]
 
 AUTHENTICATION_BACKENDS = ["auth_backend.auth.EmailBackend"]
